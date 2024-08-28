52a64b4063b66b4d0aa95156fa15eb29358705bc
navi12345679 <navithareddy879@gmail.com>
2024-08-28 14:37:40 +0530

    calculator  commit

---
+++ Index.js
@@ -0,0 +1,23 @@
+const display= document.getElementById("display");
+
+function appendTodisplay(input){
+    display.value += input;
+
+}
+
+function Clear(){
+    display.value = " ";
+}
+
+
+function Calculate(){
+try{
+    
+    display.value = eval(display.value)
+}
+catch(error){
+         display.value= "Misread"
+     
+}
+}
+ 
